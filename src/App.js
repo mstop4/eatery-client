2881a29c8532ba73a2b7b6bd1fb6703cd6812d1a
Arnold Chan <arnoldthchan@gmail.com>
2017-08-11 15:11:59 -0400

    Renders FB Button Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MyAwesomeReactComponent from './MyAwesomeReactComponent.js';
+import FBLoginButton from './FBLoginButton.js';
+
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
+        <div id="fb-root"></div>
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>React is alright</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Test</h1>
-        <h6>AAAA</h6>
-        <MuiThemeProvider>
-          <MyAwesomeReactComponent />
-        </MuiThemeProvider>
+          <FBLoginButton/>
+        <p id="status" />
       </div>
     );
   }
