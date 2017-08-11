620b8e9d0218c8ea7cbb099752f53d05c75d1a11
Arnold Chan <arnoldthchan@gmail.com>
2017-08-11 15:13:02 -0400

    webpack config supports env

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
-const path = require('path');
+const path = require(‘path’);
+
 module.exports = {
-  entry: './src/index.js',
+  entry: ‘./src/index.js’,
   output: {
-    path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    path: path.resolve(‘dist’),
+    filename: ‘index_bundle.js’
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.js$/, loader: ‘babel-loader’, exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: ‘babel-loader’, exclude: /node_modules/ }
     ]
-  }
+  },
+plugins: [
+    new DotenvPlugin({
+      sample: ‘./.env.default’,
+      path: ‘./.env’
+    })
+  ]
 }
