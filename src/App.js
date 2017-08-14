704dff149ad6b91f02faae8d435b2fb095c333bc
Arnold Chan <arnoldthchan@gmail.com>
2017-08-14 15:36:57 -0400

    removed FB SDK from app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 import MapComponent from './MapComponent.js'
@@ -13,7 +14,6 @@ import { GoogleLogin } from 'react-google-login-component';
 //Facebook Login
 import { FacebookLogin } from 'react-facebook-login-component';
 
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -24,18 +24,6 @@ class App extends Component {
       onHungryPage: true,
       onUserPage: false
     }
-  }
-
-  //Google response for login
-  responseGoogle (googleUser) {
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log({accessToken: id_token});
-    //anything else you want to do(save to localStorage)...
-  }
-  //Facebook response
-  responseFacebook (response) {
-    console.log(response);
-    //anything else you want to do(save to localStorage)...
   }
 
   handleHungryOnTap() {
@@ -52,81 +40,19 @@ class App extends Component {
     });
   }
 
-
-  componentDidMount() {
-    // This is called with the results from from window.FB.getLoginStatus().
-    function statusChangeCallback(response) {
-      console.log(response);
-      // The response object is returned with a status field that lets the
-      // app know the current login status of the person.
-      // Full docs on the response object can be found in the documentation
-      // for window.FB.getLoginStatus().
-      if (response.status === 'connected') {
-        // Logged into your app and Facebook.
-        testAPI();
-      } else {
-        // The person is not logged into your app or we are unable to tell.
-        document.getElementById('status').innerHTML = 'Please log ' +
-          'into this app.';
-      }
-    }
-    // This function is called when someone finishes with the Login
-    // Button.  See the onlogin handler attached to it in the sample
-    // code below.
-    function checkLoginState() {
-      window.FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-      });
-    }
-
-    window.fbAsyncInit = function() {
-    window.FB.init({
-      appId      : process.env.REACT_APP_FB_APPID,
-      cookie     : true,  // enable cookies to allow the server to access
-                          // the session
-      xfbml      : true,  // parse social plugins on this page
-      version    : 'v2.8' // use graph api version 2.8
-    });
-    // Now that we've initialized the JavaScript SDK, we call
-    // window.FB.getLoginStatus().  This function gets the state of the
-    // person visiting this page and can return one of three states to
-    // the callback you provide.  They can be:
-    //
-    // 1. Logged into your app ('connected')
-    // 2. Logged into Facebook, but not your app ('not_authorized')
-    // 3. Not logged into Facebook and can't tell if they are logged into
-    //    your app or not.
-    //
-    // These three cases are handled in the callback function.
-
-    window.FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-
-    };
-
-    // Load the Facebook SDK asynchronously
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-    // Here we run a very simple test of the Graph API after login is
-    // successful.  See statusChangeCallback() for when this call is made.
-    function testAPI() {
-      console.log('Welcome!  Fetching your information.... ');
-      window.FB.api('/me', function(response) {
-        console.log(response)
-        console.log('Successful login for: ' + response.name);
-        document.getElementById('status').innerHTML =
-          'Thanks for logging in, ' + response.name + '!';
-      });
-    }
-
+  responseGoogle (googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log('GOOGLE LOGIN')
+    console.log({accessToken: id_token});
+    //anything else you want to do(save to localStorage)...
   }
+
+  responseFacebook (response) {
+    console.log('FACEBOOK LOGIN')
+    console.log(response);
+    //anything else you want to do(save to localStorage)...
+  }
+
   render() {
     function logoutFacebook(){
       window.FB.getLoginStatus(function(response) {
@@ -167,6 +93,9 @@ class App extends Component {
       CurrentPage = () => (
         <User />
       );
+    }
+    function testClick(){
+      console.log('test clicked')
     }
     return (
       <div>
