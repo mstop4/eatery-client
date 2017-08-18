d17fa8e801cc1d7a7c535f46df5fe32c30f09c12
mstop4 <mstop4@gmail.com>
2017-08-18 13:58:42 -0400

    map frames points corretly

--- src/Components/MapComponent.jsx
+++ src/Components/MapComponent.jsx
@@ -5,20 +5,20 @@ import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 const params = {v: '3.exp', key: process.env.REACT_APP_GOOGLEMAPS_APIKEY, libraries: "places"};
 
-const size = 32;
+const size = {width: 24, height: 24}
 const mapicons = [
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon56.png", scaledSize: {width: size, height: size} },
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon57.png", scaledSize: {width: size, height: size} },
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon58.png", scaledSize: {width: size, height: size} },
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon59.png", scaledSize: {width: size, height: size} },
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon60.png", scaledSize: {width: size, height: size} },
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon61.png", scaledSize: {width: size, height: size} },
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon62.png", scaledSize: {width: size, height: size} },
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon63.png", scaledSize: {width: size, height: size} },
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon32.png", scaledSize: {width: size, height: size} },
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon33.png", scaledSize: {width: size, height: size} },
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon34.png", scaledSize: {width: size, height: size} },
-  { url: "http://maps.google.com/mapfiles/kml/pal5/icon35.png", scaledSize: {width: size, height: size} },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon56.png", scaledSize: size },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon57.png", scaledSize: size },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon58.png", scaledSize: size },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon59.png", scaledSize: size },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon60.png", scaledSize: size },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon61.png", scaledSize: size },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon62.png", scaledSize: size },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon63.png", scaledSize: size },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon32.png", scaledSize: size },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon33.png", scaledSize: size },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon34.png", scaledSize: size },
+  { url: "http://maps.google.com/mapfiles/kml/pal5/icon35.png", scaledSize: size },
 ]
 
 class MapComponent extends React.Component {
@@ -32,7 +32,38 @@ class MapComponent extends React.Component {
     this.onMapCreated = this.onMapCreated.bind(this)
   }
 
-  componentDidMount() {
+  componentDidUpdate() {
+
+    // create a new bounds object only if map is set up
+    if (this.state.map) {
+      let bounds = new window.google.maps.LatLngBounds(null)
+      console.dir(bounds)
+
+      // update bounds only if map is set up
+
+      const places = this.props.data
+      let n = 0
+
+      for (let place in places) {
+        if (bounds) {
+          console.dir({
+            lat: places[place]["geometry"]["location"].lat,
+            lng: places[place]["geometry"]["location"].lng
+          })
+          bounds.extend({
+            lat: places[place]["geometry"]["location"].lat,
+            lng: places[place]["geometry"]["location"].lng
+          })
+          console.dir(bounds)
+        }
+
+        if (++n >= this.props.maxResults) break
+      }
+
+      console.dir(bounds)
+      this.state.map.panToBounds(bounds)
+      this.state.map.fitBounds(bounds)
+    }
   }
 
   onMapCreated(map) {
@@ -68,15 +99,8 @@ class MapComponent extends React.Component {
     const infos = []
     let message
     let circle
-    let bounds = null
     const places = this.props.data
     let n = 0
-
-    // create a new bounds object only if map is set up
-    if (this.state.map) {
-      bounds = new window.google.maps.LatLngBounds(null)
-      console.dir(bounds)
-    }
 
     if (places && places.length > 0) {
 
@@ -97,27 +121,7 @@ class MapComponent extends React.Component {
         //     onCloseClick={this.onCloseClick} />
         //   )
 
-        // update bounds only if map is set up
-        if (bounds) {
-          console.dir({
-            lat: places[place]["geometry"]["location"].lat,
-            lng: places[place]["geometry"]["location"].lng
-          })
-          bounds.extend({
-            lat: places[place]["geometry"]["location"].lat,
-            lng: places[place]["geometry"]["location"].lng
-          })
-          console.dir(bounds)
-        }
-
         if (++n >= this.props.maxResults) break
-      }
-
-      // pan to bounds only if bounds exist
-
-      if (bounds) {
-        console.dir(bounds)
-        this.state.map.panToBounds(bounds)
       }
 
       circle = <Circle
